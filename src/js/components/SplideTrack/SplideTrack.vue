bb76734c193acecbfcc0d91590f46b7ebcbd1abb
Dan Andrade <dan@cloudpot.io>
2022-11-14 13:17:43 -0600

    Type fixes for typescript preserveValueImports and isolatedModules being enabled

--- src/js/components/SplideTrack/SplideTrack.vue
+++ src/js/components/SplideTrack/SplideTrack.vue
@@ -7,9 +7,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onUpdated, inject, Ref } from 'vue';
+import { defineComponent, onUpdated, inject, type Ref } from 'vue';
 import { SPLIDE_INJECTION_KEY } from '../../constants/keys';
-import { Splide } from '@splidejs/splide';
+import type { Splide } from '@splidejs/splide';
 
 /**
  * The component for the Splide track element.
